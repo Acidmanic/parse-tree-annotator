85a494b0d51c02a8891a136f72017f73d45b264d
Mani Moayedi <acidmanic.moayedi@gmail.com>
2023-10-16 18:19:03 +0330

    hide signature

--- acidmanic-parsetree-annotator/src/app/app.module.ts
+++ acidmanic-parsetree-annotator/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TokenGroupComponent } from './components/token-group/token-group.component';
