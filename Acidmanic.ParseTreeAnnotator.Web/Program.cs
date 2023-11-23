43fadd5fdad248b0a2df1075751dd75632e318db
Mani Moayedi <acidmanic.moayedi@gmail.com>
2023-11-23 23:37:35 +0330

    update/test backend to utilize github login

--- Acidmanic.ParseTreeAnnotator.Web/Program.cs
+++ Acidmanic.ParseTreeAnnotator.Web/Program.cs
@@ -1,19 +1,28 @@
 using Acidmanic.ParseTreeAnnotator.Web;
 using Acidmanic.ParseTreeAnnotator.Web.Extensions;
 using Acidmanic.ParseTreeAnnotator.Web.Utilities;
+using Acidmanic.Utilities.Web;
 using Acidmanic.Utilities.Web.Extensions;
 using Microsoft.Extensions.Logging.LightWeight;
 
 
 var wholeUniverseLogger = new ConsoleLogger().Shorten();
 
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.ConfigureListening("Api:Endpoints","Http");
+var frontEndApplicationUrl = builder.Configuration["FrontEndApplicationPath"];
+
+if (frontEndApplicationUrl == null)
+{
+    throw new Exception("Please set FrontEndApplicationPath in appSettings.json");
+}
+
+builder.ConfigureListening("Api:Endpoints","Http","Https");
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddApplicationPart(WebUtilities.Assembly);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -24,6 +33,9 @@ builder.Services.AddAuthentication("cookie")
     .AddCookie("cookie")
     .AddGithub(secrets["client_id"],secrets["client_secret"],"cookie");
 
+builder.Services.SetAfterLoginRedirectionUrl(frontEndApplicationUrl);
+
+builder.Services.AddWebUtilitiesServices();
 
 var app = builder.Build();
 
