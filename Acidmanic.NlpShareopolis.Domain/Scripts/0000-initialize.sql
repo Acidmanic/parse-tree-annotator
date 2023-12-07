1dfb65c155c488737cce76eae9d5320badc6c679
Mani Moayedi <acidmanic.moayedi@gmail.com>
2023-12-07 10:00:46 +0330

    Update Scripts to get pristine activities

--- Acidmanic.NlpShareopolis.Domain/Scripts/0000-initialize.sql
+++ Acidmanic.NlpShareopolis.Domain/Scripts/0000-initialize.sql
@@ -3,26 +3,19 @@
 -- {{Crud Acidmanic.NlpShareopolis.Domain.Entities.SentenceData}}
 
 -- {{ Crud Acidmanic.NlpShareopolis.Domain.Entities.UserActivity}}
--- ---------------------------------------------------------------------------------------------------------------------
 
-# CREATE PROCEDURE spReadFirstUnSeenSentence(IN UserEmail nvarchar(256))
-# BEGIN
-#     
-#     select * from SentenceDatas inner join UserActivities 
-#         on SentenceDatas.Id = UserActivities.ContributionId
-#         where UserActivities.UserEmail = 
-#     
-# 
-#     IF EXISTS(SELECT 1 FROM SentenceDatas WHERE SentenceDatas.Id = Id) then
-# 
-#         UPDATE SentenceDatas SET Text=Text,LanguageShortName=LanguageShortName WHERE SentenceDatas.Id = Id;
-# 
-#         SELECT * FROM SentenceDatas WHERE SentenceDatas.Id = Id ORDER BY Id ASC LIMIT 1;
-# 
-#     ELSE
-#         INSERT INTO SentenceDatas (Text,LanguageShortName,Id) VALUES (Text,LanguageShortName,Id);
-#         SET @nid = Id;
-#         SELECT * FROM SentenceDatas WHERE SentenceDatas.Id = @nid;
-#     END IF;
-# END;
-# -- ---------------------------------------------------------------------------------------------------------------------
+-- ---------------------------------------------------------------------------------------------------------------------
+create function fnIsPristine(Id binary(16), UserEmail nvarchar(256)) returns bit DETERMINISTIC
+
+    return not exists(select count(*) from UserActivities 
+                                      where 
+                                        UserActivities.ContributionId = Id
+                                        AND UserActivities.UserEmail = UserEmail
+                                        AND UserActivities.Status !=0 );
+-- ---------------------------------------------------------------------------------------------------------------------
+CREATE PROCEDURE spReadFirstUnSeenSentence(IN UserEmail nvarchar(256))
+BEGIN
+    
+    select * from SentenceDatas where fnIsPristine(SentenceDatas.Id,UserEmail) limit 1; 
+END;
+-- ---------------------------------------------------------------------------------------------------------------------
