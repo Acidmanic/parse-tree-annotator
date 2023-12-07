78296da915d932c3d273fe0364fe075b8d32b085
Mani Moayedi <acidmanic.moayedi@gmail.com>
2023-12-07 13:41:14 +0330

    configure entier resolver

--- Acidmanic.NlpShareopolis.Api/Program.cs
+++ Acidmanic.NlpShareopolis.Api/Program.cs
@@ -48,6 +48,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.ConfigureEnTierResolver();
 app.ConfigureMeadow();
 
 var frontEndApplication = new StaticServerConfigurator().ServeForAngular().UseLogger(wholeUniverseLogger);
