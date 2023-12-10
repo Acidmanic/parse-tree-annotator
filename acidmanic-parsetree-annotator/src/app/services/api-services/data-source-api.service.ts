beddea58b8eb49a988ecdda261df238b25a89142
Mani Moayedi <acidmanic.moayedi@gmail.com>
2023-12-11 00:56:12 +0330

    Add DataSourceApiService to front

---
+++ acidmanic-parsetree-annotator/src/app/services/api-services/data-source-api.service.ts
@@ -0,0 +1,52 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {Observable, Subject} from "rxjs";
+import {BaseUrlService} from "../base-url.service";
+import {UserInformationModel} from "../../models/user-information.model";
+import {ResultModel} from "../../models/result.model";
+import {SentenceTaskModel} from "../../models/api/sentence-task.model";
+import {ParsedTreeModel} from "../../models/api/parsed-tree.model";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataSourceApiService {
+
+
+  public static readonly fetchSentenceUri='data-source/fetch-sentence/';
+  public static readonly skipSentenceUri='data-source/skip-sentence/';
+  public static readonly deliverSentenceUri='data-source/deliver-parsed-tree';
+
+  constructor(private http:HttpClient) {
+  }
+
+
+  public fetchSentence(languageShortname:string):Observable<ResultModel<SentenceTaskModel>>{
+
+    let url = BaseUrlService.authBaseUrl + '/' + DataSourceApiService.fetchSentenceUri+languageShortname.toLowerCase();
+
+    return this.http.get<ResultModel<SentenceTaskModel>>(url);
+
+  }
+
+  public skipSentence(sentenceId:string):Observable<ResultModel<SentenceTaskModel>>{
+
+    let url = BaseUrlService.authBaseUrl + '/' + DataSourceApiService.skipSentenceUri+sentenceId;
+
+    return this.http.put<ResultModel<SentenceTaskModel>>(url,{});
+  }
+
+  public deliverSentence(sentenceId:string,parsedTree:string,languageShortName:string):Observable<ResultModel<SentenceTaskModel>>{
+
+    let url = BaseUrlService.authBaseUrl + '/' + DataSourceApiService.deliverSentenceUri;
+
+    let model = new ParsedTreeModel();
+    model.sentenceId=sentenceId;
+    model.parsedTree=parsedTree;
+    model.languageShortName=languageShortName;
+
+    return this.http.post<ResultModel<SentenceTaskModel>>(url,model);
+  }
+
+}
